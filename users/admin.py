1d29b711a052b28bfa27e2f2d8c98fe158a1da34
hbkm12 <hbkm12@gmail.com>
2021-09-19 11:16:38 +0900

    0912 #3 USER APP 까지

---
+++ users/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.contrib.admin.filters import FieldListFilter
+from django.contrib.auth.admin import UserAdmin
+from . import models
+
+# Register your models here.
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+
+    """Custom User Admin"""
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthday",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
