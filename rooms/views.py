73b646e2cf1af07de2e9cea58146d77c9d424c1a
hbkm12 <hbkm12@gmail.com>
2021-09-28 22:34:08 +0900

    [210928] #10. introduction to views and urls 완료

--- rooms/views.py
+++ rooms/views.py
@@ -1,16 +1,50 @@
-from django.shortcuts import render
-from datetime import date, datetime
+from django.views.generic import ListView
+from django.utils import timezone
 from . import models
 
 # Create your views here.
 
 
-def all_rooms(request):
-    all_rooms = models.Room.objects.all()
-    return render(
-        request,
-        "rooms/home.html",
-        context={
-            "rooms": all_rooms,
-        },
-    )
+class HomeView(ListView):
+    """HomeView Definition"""
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
+
+
+# # paginator way
+# def all_rooms(request):
+#     page = request.GET.get("page", 1)
+#     room_list = models.Room.objects.all()
+#     paginator = Paginator(room_list, 10, orphans=5)
+#     # page = int(page or 1)
+#     # page_size = 10
+#     # limit = page_size * page
+#     # offset = limit - page_size
+#     # all_rooms = models.Room.objects.all()[offset:limit]
+#     # page_count = ceil(models.Room.objects.count() / page_size)
+#     try:
+#         rooms = paginator.page(int(page))
+#         return render(
+#             request,
+#             "rooms/home.html",
+#             {
+#                 "page": rooms,
+#                 # "rooms": all_rooms,
+#                 # "page": page,
+#                 # "page_count": page_count,
+#                 # "page_range": range(1, page_count),
+#             },
+#         )
+
+#     except EmptyPage:
+#         return redirect("/")
